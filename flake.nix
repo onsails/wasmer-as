3eb842ffa70fe7d773df6c538eed2cc7cfae7499
Andrey Kuznetsov <fear@loathing.in>
2021-07-18 00:06:49 +0300

    nix flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    utils.url = github:numtide/flake-utils;
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, utils, rust-overlay }: utils.lib.eachDefaultSystem (
+    system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlay ];
+        };
+      in
+        {
+
+          devShell = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              rust-bin.stable.latest.default
+              libiconv
+            ];
+          };
+
+        }
+  );
+}
